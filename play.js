54d2832b9d148b2633330b10f26ac7238d1aa272
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-07-13 20:09:31 +0100

    struggling to get player input:

--- play.js
+++ play.js
@@ -4,11 +4,31 @@ var Hero = require('./hero');
 var Food = require('./food');
 var Rat = require('./rat');
 
+var readline = require('readline');
+
+
+
 var game = new Game();
 var ratKing = new Baddy( "Rat King", 100, "Chicken Drumsticks"  );
 var steve = new Hero( "Steve", 100, "Chicken Drumsticks" );
 var chicken = new Food( "Chicken Drumsticks", 10 );
 var toast = new Food( "Toast", 3 );
 var bob = new Rat( "bob", true );
-var game.start( steve, ratKing );
+game.start( steve, ratKing );
+
+var run = function() {
+  while ( game.won() === false ) {
+    
+  }
+}
+
+
+
+
+
+
+
+
+
+
 
